b0f4bbe6ccdf31c8572a44b27647a67079775367
christian <hernandezchristian94@gmail.com>
2024-08-07 16:00:20 -0700

    first commit

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ContactForm.css";
 
 function ContactForm() {
+  const [isVisible, setIsVisible] = useState(false);
+  const observer = useRef(null);
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null, // viewport
+        rootMargin: "0px",
+        threshold: 0.5, // 0 (completely out of view) to 1 (completely in view)
+      }
+    );
+
+    if (contactRef.current) {
+      observer.current.observe(contactRef.current);
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, []);
   return (
-    <div className="ContactForm">
+    <div ref={contactRef} className="ContactForm">
       <div className="contact-form-wrapper">
         <div className="contact-title">
-          <h1>Schedule Your Consultation Today</h1>
+          <h1
+            style={{ top: isVisible ? 0 : "40px", opacity: isVisible ? 1 : 0 }}
+          >
+            Schedule Your Consultation Today
+          </h1>
         </div>
-        <form>
+        <form
+          style={{ right: isVisible ? 0 : "40px", opacity: isVisible ? 1 : 0 }}
+        >
           <div className="full-name">
             <input type="text" placeholder="First name" />
             <input type="text" placeholder="Last name" />
